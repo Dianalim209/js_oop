61ceb0b626caaf4252e4096466c985040848b234
Dianalim209 <dianamlim209@yahoo.com>
2016-06-08 19:17:17 -0700

    add file

--- creating_obj_p1/create_obj_p1.js
+++ creating_obj_p1/create_obj_p1.js
@@ -1,20 +1,38 @@
-function NinjaConstructor(name, age, prevOccupation){
-  var ninja = {};
-  ninja.name = name;
-  ninja.age = age;
-  ninja.prevOccupation = prevOccupation
-  ninja.introduce = function()  {
-    console.log("Hi my name is " + ninja.name + ". I used to be a "+ ninja.prevOccupation + " and now I'm a ninja! :p " + "And I am " + ninja.age + " years old!");
-  }
-  return ninja;
-}
+// Create a vehicleConstructor that takes in the name, number of wheels, and number of passengers
+// Each vehicle should have a makeNoise method
+// Using the constructor, create a Bike
+// redefine the Bike's makeNoise method to print "ring ring!"
+// Using the constructor, create a Sedan
+// redefine the Sedan's makeNoise method to print "Honk Honk!"
+// Using the constructor, create a Bus
+// Give the bus a pickUpPassengers method that takes in the number of passengers to pick up and adds them to the passenger count
 
-var Diana = NinjaConstructor("Diana", 24, "Marine");
-Diana.introduce = function() {
-  console.log("I am the one!")
+function VehicleConstructor(name, wheels, passengers){
+  var vehicle = {};
+  vehicle.name = name;
+  vehicle.wheels = wheels;
+  vehicle.passengers = passengers;
+  vehicle.makeNoise = function(){
+    console.log("I am a " + vehicle.name + ". I have " + vehicle.wheels + " wheels. And I can carry " + vehicle.passengers + " passenger(s).")
+  }; return vehicle;
 }
-var Michael = NinjaConstructor("Michael", 34, "Founder");
-Michael.introduce = function() {
-  console.log("I am the Sensei!")
-}
-Michael.introduce();
+var Bike = VehicleConstructor('Bike',2,1);
+console.log(Bike);
+Bike.makeNoise = function(){console.log("ring ring!")};
+Bike.makeNoise();
+
+var Sedan = VehicleConstructor('Sedan', 4, 4);
+console.log(Sedan);
+Sedan.makeNoise = function(){console.log("Honk Honk! Move out the way!")};
+Sedan.makeNoise();
+
+var Bus = VehicleConstructor('Bus',6,0);
+console.log(Bus);
+Bus.pickUpPassenger = function(){this.passengers++};
+console.log(Bus.passengers);
+Bus.pickUpPassenger();
+console.log(Bus.passengers);
+Bus.pickUpPassenger();
+console.log(Bus.passengers);
+Bus.pickUpPassenger();
+console.log(Bus.passengers);
